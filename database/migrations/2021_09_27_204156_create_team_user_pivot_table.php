22b90ae676ee55db754c5a914e3ffbedc3778940
Dominik Hrinkino <admin@hrinkino.com>
2021-09-27 23:00:47 +0200

    team-user pivot

---
+++ database/migrations/2021_09_27_204156_create_team_user_pivot_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateTeamUserPivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('team_user', function (Blueprint $table) {
+            $table->unsignedBigInteger('team_id')->index();
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id')->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->primary(['team_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('team_user');
+    }
+}
