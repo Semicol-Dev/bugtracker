79d58fcce05f08714df7c8cf76af5843069c2b20
Dominik Hrinkino <admin@hrinkino.com>
2021-10-02 16:48:30 +0200

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TeamController;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\IssueController;
+use App\Http\Controllers\FileController;
+
+use GuzzleHttp\Middleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,8 +24,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::post('issue/{id}/note', ['App\Http\Controllers\IssueController','note'])->middleware('auth');
+Route::post('file/upload', ['App\Http\Controllers\FileController','store'])->middleware('auth');
+Route::get('file/{id}', ['App\Http\Controllers\FileController','get'])->middleware('auth');
+Route::get('file/{id}/delete', ['App\Http\Controllers\FileController','destroy'])->middleware('auth');
+
+Route::get('issue/note/delete/{id}', ['App\Http\Controllers\IssueController','note_delete'])->middleware('auth');
+
 Route::resource('project', ProjectController::class)->middleware('auth');
-
 Route::resource('team', TeamController::class)->middleware('auth');
-
+Route::resource('issue', IssueController::class)->middleware('auth');
 Route::resource('home', HomeController::class)->middleware('auth');
+Route::get('admin', ['App\Http\Controllers\AdminController','index'])->middleware('isAdmin');
