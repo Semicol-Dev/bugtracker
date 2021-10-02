d24687b4b76147bc0b063138e4e178e840f546e2
Dominik Hrinkino <admin@hrinkino.com>
2021-10-02 19:24:23 +0200

    saved changes

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,8 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
 </head>
 <body>
+    <ul>
+        <li><a href="/issue">Issues</a></li>
+        <li><a href="/team">Teams</a></li>
+        <li><a href="/project">Projects</a></li>
+    </ul>
+    @foreach (auth()->user()->all_issues() as $issue)
+        {{$issue}};
+    @endforeach
 </body>
 </html>
