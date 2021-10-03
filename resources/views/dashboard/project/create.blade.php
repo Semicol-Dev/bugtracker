1ffc4871ba9eb1ab9866947d4cc3084ed9ff0a84
Dominik Hrinkino <admin@hrinkino.com>
2021-10-03 12:58:09 +0200

    created if statement when there is no team all inputs are disabled

--- resources/views/dashboard/project/create.blade.php
+++ resources/views/dashboard/project/create.blade.php
@@ -2,15 +2,20 @@
     <!-- CSRF TOKEN, ten tu musi byt lebo CSRF protection je by default zapnuta pri kazdom requeste inak error 413 -->
     @csrf
     <p>Nazov Projektu</p>
-    <input type="text" name="name" required><br />
+    <input type="text" name="name" required @if ($teams->count() == 0) disabled @endif><br />
     <p>Vyber teamu</p>
-
+     
+    @if (($teams->count() == 0))
+        <p>Nemozete vytvorit projekt bez toho aby ste vytvorili najprv team, prosim <a href="/team/create">vytvorte team</a></p>
+    @else
     <select name="team">
         @foreach ($teams as $team)
             <option value="{{ $team->id }}" selected>{{ $team->name }}</option>
         @endforeach
     </select>
+    @endif
+
 
     <br>
-    <input type="submit" value="Vytvorit projekt">
+    <input @if ($teams->count() == 0) disabled @endif type="submit" value="Vytvorit projekt">
 </form>
