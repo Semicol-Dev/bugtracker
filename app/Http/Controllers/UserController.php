480b39a026dd1a3792a7dd079451c2d5bf616607
Dominik Hrinkino <admin@hrinkino.com>
2021-10-16 12:29:05 +0200

    fixed view in index

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        return view('dashboard.index');
+        return view('dashboard.user.index');
     }
 
     /**
