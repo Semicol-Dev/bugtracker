e8d7c0b9532112a2b3a10c1e5fda1939f20e8afb
cutegrim <48733615+cutegrim@users.noreply.github.com>
2021-10-20 06:52:29 +0200

    Issue display hopefully finished. Sorting works nice.

--- public/js/issueList.js
+++ public/js/issueList.js
@@ -3,34 +3,31 @@ $("#all").show();
 $("#casual").hide();
 $("#feature").hide();
 
+function hideAll() {
+    $("#critical").hide();
+    $("#all").hide();
+    $("#casual").hide();
+    $("#feature").hide();
+    $("#solved").hide();
+}
+
 function listFilter(type) {
 
-    switch (type) {
-        case 0:
-            $("#all").show();
-            $("#casual").hide();
-            $("#feature").hide();
-            $("#critical").hide();
-            break;
-        case 1:
-            $("#all").hide();
-            $("#casual").hide();
-            $("#feature").hide();
-            $("#critical").show();
-            break;
-        case 2:
-            $("#all").hide();
-            $("#casual").show();
-            $("#feature").hide();
-            $("#critical").hide();
-            break;
-        case 3:
-            $("#all").hide();
-            $("#casual").hide();
-            $("#feature").show();
-            $("#critical").hide();
-            break;
+    if (type == 0) {
+        hideAll();
+        $("#all").show();
+    } else if (type == 1) {
+        hideAll();
+        $("#critical").show();
+    } else if (type == 2) {
+        hideAll();
+        $("#casual").show();
+    } else if (type == 3) {
+        hideAll();
+        $("#feature").show();
+    } else if (type == 4) {
+        hideAll();
+        $("#solved").show();
     }
-
 
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -9,41 +9,93 @@
     <button type="button" class="btn btn-danger btn-square-template" onclick="listFilter(1)"><h2>{{auth()->user()->type_issue(1)->count()}}</h2><h5>Critical</h5></button>
     <button type="button" class="btn btn-warning btn-square-template"onclick="listFilter(2)"><h2>{{auth()->user()->type_issue(2)->count()}}</h2><h5>Casual</h5></button>
     <button type="button" class="btn btn-secondary btn-square-template"onclick="listFilter(3)"><h2>{{auth()->user()->type_issue(3)->count()}}</h2><h5>Feature</h5></button>
-    <button type="button" class="btn btn-success btn-square-template"><h2>25</h2><h5>Solved</h5></button>
+    <button type="button" class="btn btn-success btn-square-template"onclick="listFilter(4)"><h2>{{auth()->user()->solved_issues(false)->count()}}</h2><h5>Solved</h5></button>
 </nav>
-@foreach (auth()->user()->my_issues() as $issue)
-{{$issue->title}} <!-- title of ticket-->
-{{Str::limit($issue->description, 20)}} <!-- trim description to 20 chars-->
-{{$issue->type}} <!-- 1 Critical, 2 Casual, 3 Feature-->
-{{$issue->status}} <!-- 1 = solved -->
-{{$issue->assigned->name}} <!-- who have assigned ticket -->
-{{$issue->created_by->name}} <!-- who created a ticket-->
-{{$issue->created_at}} <!-- when ticket was made -->
-{{$issue->updated_at}} <!-- last update -->
-
-@endforeach
-<div id="all">
+<br>
+<div id="all" style="display:none">
     @foreach (auth()->user()->my_issues() as $issue)
-    <h1>{{$issue->title}}</h1>
-    <h3>{{$issue->description}}</h3>
+    @if ($issue->type == 1)
+    <div class="issue-list">
+        <h1>{{$issue->title}} <span class="badge  badge-pill badge-danger">{{$issue->project->name}}</span></h1>
+        <h4>{{$issue->description}}</h4>
+        <h6>Created: {{$issue->created_at}}</h6>
+        <hr style="background-color: #ffff">
+    </div>
+@endif
+@if ($issue->type == 2)
+    <div class="issue-list">
+        <h1>{{$issue->title}} <span class="badge  badge-pill badge-warning">{{$issue->project->name}}</span></h1>
+        <h4>{{$issue->description}}</h4>
+        <h6>Created: {{$issue->created_at}}</h6>
+        <hr style="background-color: #ffff">
+    </div>
+@endif
+@if ($issue->type == 3)
+    <div class="issue-list">
+        <h1>{{$issue->title}} <span class="badge  badge-pill badge-secondary">{{$issue->project->name}}</span></h1>
+        <h4>{{$issue->description}}</h4>
+        <h6>Created: {{$issue->created_at}}</h6>
+        <hr style="background-color: #ffff">
+    </div>
+@endif
     @endforeach
 </div>
-<div id="critical">
+<div id="critical" style="display:none">
     @foreach (auth()->user()->type_issue(1) as $issue)
-    <h1>{{$issue->title}}</h1>
-    <h3>{{$issue->description}}</h3>
+        <div class="issue-list">
+            <h1>{{$issue->title}} <span class="badge  badge-pill badge-danger">{{$issue->project->name}}</span></h1>
+            <h4>{{$issue->description}}</h4>
+            <h6>Created: {{$issue->created_at}}</h6>
+            <hr style="background-color: #ffff">
+        </div>
     @endforeach
 </div>
-<div id="casual">
+<div id="casual" style="display:none">
     @foreach (auth()->user()->type_issue(2) as $issue)
-    <h1>{{$issue->title}}</h1>
-    <h3>{{$issue->description}}</h3>
+        <div class="issue-list">
+            <h1>{{$issue->title}} <span class="badge  badge-pill badge-warning">{{$issue->project->name}}</span></h1>
+            <h4>{{$issue->description}}</h4>
+            <h6>Created: {{$issue->created_at}}</h6>
+            <hr style="background-color: #ffff">
+        </div>
     @endforeach
 </div>
-<div id="feature">
+<div id="feature" style="display:none">
     @foreach (auth()->user()->type_issue(3) as $issue)
-    <h1>{{$issue->title}}</h1>
-    <h3>{{$issue->description}}</h3>
+        <div class="issue-list">
+            <h1>{{$issue->title}} <span class="badge  badge-pill badge-secondary">{{$issue->project->name}}</span></h1>
+            <h4>{{$issue->description}}</h4>
+            <h6>Created: {{$issue->created_at}}</h6>
+            <hr style="background-color: #ffff">
+        </div>
+    @endforeach
+</div>
+<div id="solved" style="display:none">
+    @foreach (auth()->user()->solved_issues(false) as $issue)
+        @if ($issue->type == 1)
+            <div class="issue-list">
+                <h1>{{$issue->title}} <span class="badge  badge-pill badge-danger">{{$issue->project->name}}</span><span class="badge  badge-pill badge-success">Solved</span></h1>
+                <h4>{{$issue->description}}</h4>
+                <h6>Created: {{$issue->created_at}}</h6>
+                <hr style="background-color: #ffff">
+            </div>
+        @endif
+        @if ($issue->type == 2)
+            <div class="issue-list">
+                <h1>{{$issue->title}} <span class="badge  badge-pill badge-warning">{{$issue->project->name}}</span><span class="badge  badge-pill badge-success">Solved</span></h1>
+                <h4>{{$issue->description}}</h4>
+                <h6>Created: {{$issue->created_at}}</h6>
+                <hr style="background-color: #ffff">
+            </div>
+        @endif
+        @if ($issue->type == 3)
+            <div class="issue-list">
+                <h1>{{$issue->title}} <span class="badge  badge-pill badge-secondary">{{$issue->project->name}}</span><span class="badge  badge-pill badge-success">Solved</span></h1>
+                <h4>{{$issue->description}}</h4>
+                <h6>Created: {{$issue->created_at}}</h6>
+                <hr style="background-color: #ffff">
+            </div>
+        @endif
     @endforeach
 </div>
 @endsection
