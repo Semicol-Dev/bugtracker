9e400692f360980477b18ad7d21561ce8239bf1d
cutegrim <48733615+cutegrim@users.noreply.github.com>
2021-10-26 20:38:13 +0200

    Teams top set

--- resources/views/dashboard/team/index.blade.php
+++ resources/views/dashboard/team/index.blade.php
@@ -1,4 +1,11 @@
-<h1>Vsetky Teamy</h1>
+@extends('baseSideNav')
+@section('core')
+
+<nav class="navbar navbar-dark issue-list">
+    <h1>Teams</h1>
+    <a class="btn btn-info btn-bug-template " style="color: #121212" type="submit" href="/team/create" ><i class="bi bi-bug-fill"></i>New Team</a>
+</nav>
+
 <a href="/team/create">Vytvorit novy team</a>
 <table border=1>
     <tr>
@@ -15,4 +22,4 @@
     @endforeach
 </table>
 
-
+@endsection
