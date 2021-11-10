91c9f450235d45bb3058ccc7ce5cd80d0bcd2c35
cutegrim <48733615+cutegrim@users.noreply.github.com>
2021-11-10 12:22:14 +0100

    Code made nicer

--- resources/views/dashboard/issue/show.blade.php
+++ resources/views/dashboard/issue/show.blade.php
@@ -9,67 +9,62 @@ $loopCount = 0
     <div> 
         <h1 class="display-4">{{ $issue->title }}</h1>
         @if ($issue->type == 2)
-    <div class="issue-list">
-        <h2><span class="badge  badge-pill badge-warning">Casual</span></h2>
-        
-    </div>
-@endif
-@if ($issue->type == 3)
-    <div class="issue-list">
-        <h2><span class="badge  badge-pill badge-secondary">Feature</span></h2>
-        
-    </div>
-@endif
-    </div>
-    @if ($issue->type == 1)
-    <div class="issue-list">
-        <h2><span class="badge  badge-pill badge-danger">Critical</span></h2>
-        
-    </div>
-@endif
-    <div style="width: 50%; ">
-        <div class="row  d-flex justify-content-end">
-        @if (auth()->user()->isAdmin() || auth()->user()->isDev())
-        <form action="/issue/{{$issue->id}}/assign" method="post">
-            @csrf
-            <input type="hidden" name="user_id" value="{{auth()->user()->id}}">
-            <button type="submit" class="btn btn-info btn-bug-template" style="color: #101010">
-                <i class="bi bi-trophy-fill"></i>Take
-            </button>
-        </form>
-    @endif
-        </div>
-    <div class="row pt-2  1d-flex justify-content-end" >
-        @if ($issue->created_user_id == auth()->user()->id ||
-        auth()->user()->isAdmin())
-        <form id="delete" method="post" action="/issue/{{ $issue->id }}">
-            @method('delete')
-            @csrf
-            <button onclick="document.getElementById('delete').submit();" class="btn btn-danger btn-bug-template" >
-                <i class="bi bi-trash-fill"></i>Delete
-            </button>
-        </form>
-    @endif
-    </div>
-    <div class="row pt-1 d-flex justify-content-end" >
-        @if (auth()->user()->isAdmin())
-            <form action="/issue/{{$issue->id}}/assign" method="post"  >
-            @csrf
-        <select name="user_id" class="sm-form-input" style="width: 10rem">
-            @foreach ($issue->project->team->users as $user)
-                <option value="{{$user->id}}">{{$user->name}}</option>
-            @endforeach
-        </select>
-        <button type="submit" class="btn btn-info btn-bug-template" style="color: #101010">
-            <i class="bi bi-megaphone-fill"></i>Asign
-        </button>
-       
-    </form>
-@endif
+            <div class="issue-list">
+                <h2><span class="badge  badge-pill badge-warning">Casual</span></h2>
+            </div>
+        @endif
+        @if ($issue->type == 3)
+            <div class="issue-list">
+                <h2><span class="badge  badge-pill badge-secondary">Feature</span></h2>
+            </div>
+        @endif
+        @if ($issue->type == 1)
+            <div class="issue-list">
+                <h2><span class="badge  badge-pill badge-danger">Critical</span></h2>
+            </div>
+        @endif
     </div>
     
+    <div style="width: 50%; ">
+        <div class="row  d-flex justify-content-end">
+            @if (auth()->user()->isAdmin() || auth()->user()->isDev())
+                <form action="/issue/{{$issue->id}}/assign" method="post">
+                    @csrf
+                    <input type="hidden" name="user_id" value="{{auth()->user()->id}}">
+                    <button type="submit" class="btn btn-info btn-bug-template" style="color: #101010">
+                        <i class="bi bi-trophy-fill"></i>Take
+                    </button>
+                </form>
+            @endif
+        </div>
+        <div class="row pt-2  1d-flex justify-content-end" >
+            @if ($issue->created_user_id == auth()->user()->id ||
+            auth()->user()->isAdmin())
+                <form id="delete" method="post" action="/issue/{{ $issue->id }}">
+                    @method('delete')
+                    @csrf
+                    <button onclick="document.getElementById('delete').submit();" class="btn btn-danger btn-bug-template" >
+                        <i class="bi bi-trash-fill"></i>Delete
+                    </button>
+                </form>
+            @endif
+        </div>
+        <div class="row pt-1 d-flex justify-content-end" >
+            @if (auth()->user()->isAdmin())
+                <form action="/issue/{{$issue->id}}/assign" method="post"  >
+                    @csrf
+                    <select name="user_id" class="sm-form-input" style="width: 10rem">
+                        @foreach ($issue->project->team->users as $user)
+                            <option value="{{$user->id}}">{{$user->name}}</option>
+                        @endforeach
+                    </select>
+                    <button type="submit" class="btn btn-info btn-bug-template" style="color: #101010">
+                        <i class="bi bi-megaphone-fill"></i>Asign
+                    </button>
+                </form>
+            @endif
+        </div>
    </div>
-   
 </nav>
 <br>
 <div class="issue-list-container issue-list"  style="height: calc(100vh - 265px)">
@@ -86,8 +81,7 @@ $loopCount = 0
             </td>
             <td class="bug-show-info-td">
                 <b>Project:</b> {{ $issue->project->name }}
-            </td>
-            
+            </td> 
         </tr>
     </table>
     <table>
@@ -102,12 +96,7 @@ $loopCount = 0
     </table>
     <h3>Bug Description</h3>
     <div class="bug-show-info-description">{{ $issue->description }}</div>
-    
-    
-    
-    
     <hr>
-    
     <div>
         <h4>Add Comment</h4>
         <form class="row d-flex justify-content-between"  style="width: 100%;" method="post" name="comments_form" id="comments_form" action="/issue/{{ $issue->id }}/note">
@@ -118,7 +107,6 @@ $loopCount = 0
                     Intern Comment: <input type="checkbox" name="hidden_comment" id=""><br />        
                 @endif
            </div>
-        
            <div class="col-3 d-flex justify-content-end pr-1" > 
                 <button type="submit" class="btn btn-info btn-bug-template align-self-center " style="color: #101010">
                     <i class="bi bi-chat-right-text-fill"></i>Post
@@ -126,8 +114,6 @@ $loopCount = 0
             </div>
         </form>
     </div>
-   
-    
     <hr>
     <h4>Uploaded Files</h4>
     @if (auth()->user()->isAdmin() ||
@@ -135,7 +121,6 @@ $loopCount = 0
         <form class="row d-flex justify-content-between"  style="width: 100%;" action="/file/upload" method="post" enctype="multipart/form-data">
             @csrf
             <div class="col-4">
-               
                 <label class="btn btn-info btn-bug-template" style="color: #101010">
                     <input type="file" name="file" id="">
                     <i class="bi bi-file-earmark-fill"></i>Choose File
@@ -149,17 +134,13 @@ $loopCount = 0
             </div>
         </form>
     @endif
-    
     <ul >
         @foreach ($issue->files as $file)
             <li><b><a class="def-link link-info" href="/file/{{ $file->id }}">{{ $file->text }}</a></b> - {{ $file->user->name }} -
                 {{ $file->created_at }}
-                @if (auth()->user()->isAdmin() ||
-        auth()->user()->id == $file->user_id)
-                    - <a class="badge badge-danger" href="/file/{{ $file->id }}/delete"> Delete</a> 
-                    
+                @if (auth()->user()->isAdmin() || auth()->user()->id == $file->user_id)
+                    - <a class="badge badge-danger" href="/file/{{ $file->id }}/delete"> Delete</a>   
                 @endif
-    
             </li>
         @endforeach
     </ul>
@@ -167,42 +148,34 @@ $loopCount = 0
     <h4>Comments</h4>
     @foreach ($issue->comments as $comment)
         @if ($comment->internal)
-            @if (auth()->user()->isDev() ||
-        auth()->user()->isAdmin())
-                
-                <div class="pb-3 bug-show-info-description" style="background-color: #323232">
-                    <h5>
-                        <b>{{ $comment->user->name }}</b>
-                        <small>
-                            -Intern Comment-
-                            {{ $comment->created_at }}
-                            <a class="badge badge-danger" href="/issue/note/delete/{{ $comment->id }}">Delete</a>
-                        </small>
-                    </h5>
-                    
-                    {{ $comment->text }}
-                </div>
-               <br>
-                
-            @endif
-        @else
-           
-        <div class="pb-3 bug-show-info-description" style="background-color: #323232">
-            <h5>
-                <b>{{ $comment->user->name }}</b>
-                <small>
+        @if (auth()->user()->isDev() || auth()->user()->isAdmin())
+            <div class="pb-3 bug-show-info-description" style="background-color: #323232">
+                <h5>
+                    <b>{{ $comment->user->name }}</b>
+                    <small>
+                        -Intern Comment-
+                        {{ $comment->created_at }}
+                        <a class="badge badge-danger" href="/issue/note/delete/{{ $comment->id }}">Delete</a>
+                    </small>
+                </h5>
+                {{ $comment->text }}
+            </div>
+            <br>    
+        @endif
+        @else   
+            <div class="pb-3 bug-show-info-description" style="background-color: #323232">
+                <h5>
+                    <b>{{ $comment->user->name }}</b>
+                    <small>
                     {{ $comment->created_at }}
                     <a class="badge badge-danger" href="/issue/note/delete/{{ $comment->id }}">Delete</a>
-                </small>
-            </h5>
-            
-            {{ $comment->text }}
-        </div>
-        <br>
+                    </small>
+                </h5>
+                {{ $comment->text }}
+            </div>
+            <br>
         @endif
     @endforeach
-   </h3>
-   
 </div>
 @endsection
 
