1f2e057241aa3ff6afb529de4c1352d9c5b324c3
cutegrim <48733615+cutegrim@users.noreply.github.com>
2021-11-23 11:14:17 +0100

    All issue index created kinda :D

--- resources/views/dashboard/issue/index.blade.php
+++ resources/views/dashboard/issue/index.blade.php
@@ -1,40 +1,70 @@
-<a href="/issue/create">Vytvorit novy ticket</a>
-<table border="3">
-@foreach ($issues as $issue)
-    <tr>
-        <td>
-            <a href="/issue/{{$issue->id}}">{{$issue->title}}</a>
-        </td>
-        <td>
-            @switch($issue->type)
-                @case(1)
-                    Critical
-                    @break
-                @case(2)
-                    Casual                    
-                    @break
-                @case(3)
-                    Feature
-                    @break
-                @default
-                    
-            @endswitch
-        </td>
-        <td>
-            @if ($issue->status)
-                Ukonceny
+@extends('baseSideNav')
+@section('title')
+    index
+@endsection
+@section('core')
+<br>
+<nav class="navbar navbar-dark issue-list">
+    <h1 class="display-4">All Tickets</h1>
+</nav>
+<br>
+<div class="issue-list-container">
+    <div>
+        @foreach (auth()->user()->my_issues() as $issue)
+            @if ($issue->type == 1)
+                <div class="issue-list">
+                    <h1><a class="def-link link-info" href="/issue/{{$issue->id}}">{{$issue->title}}</a> <span class="badge  badge-pill badge-danger">{{$issue->project->name}}</span></h1>
+                    <h4>{{$issue->description}}</h4>
+                    <h6>Created: {{$issue->created_at}}</h6>
+                    <hr>
+                </div>
             @endif
-        </td>
-        <td>
-            {{$issue->updated_at}}
-        </td>
-    </tr>
-    <tr>
-        <td>
-            @php
-                echo substr($issue->description, 0, 100);
-            @endphp
-        </td>
-    </tr>              
-@endforeach
-</table>
+            @if ($issue->type == 2)
+                <div class="issue-list">
+                    <h1><a class="def-link link-info" href="/issue/{{$issue->id}}">{{$issue->title}}</a> <span class="badge  badge-pill badge-warning">{{$issue->project->name}}</span></h1>
+                    <h4>{{$issue->description}}</h4>
+                    <h6>Created: {{$issue->created_at}}</h6>
+                    <hr>
+                </div>
+            @endif
+            @if ($issue->type == 3)
+                <div class="issue-list">
+                    <h1><a class="def-link link-info" href="/issue/{{$issue->id}}">{{$issue->title}}</a> <span class="badge  badge-pill badge-secondary">{{$issue->project->name}}</span></h1>
+                    <h4>{{$issue->description}}</h4>
+                    <h6>Created: {{$issue->created_at}}</h6>
+                    <hr>
+                </div>
+            @endif
+        @endforeach
+    </div>
+    <div>
+        @foreach (auth()->user()->solved_issues(false) as $issue)
+            @if ($issue->type == 1)
+                <div class="issue-list">
+                    <h1><a class="def-link link-info" href="/issue/{{$issue->id}}">{{$issue->title}}</a> <span class="badge  badge-pill badge-danger">{{$issue->project->name}}</span><span class="badge  badge-pill badge-success">Solved</span></h1>
+                    <h4>{{$issue->description}}</h4>
+                    <h6>Created: {{$issue->created_at}}</h6>
+                    <hr>
+                </div>
+            @endif
+            @if ($issue->type == 2)
+                <div class="issue-list">
+                    <h1><a class="def-link link-info" href="/issue/{{$issue->id}}">{{$issue->title}}</a> <span class="badge  badge-pill badge-warning">{{$issue->project->name}}</span><span class="badge  badge-pill badge-success">Solved</span></h1>
+                    <h4>{{$issue->description}}</h4>
+                    <h6>Created: {{$issue->created_at}}</h6>
+                    <hr>
+                </div>
+            @endif
+            @if ($issue->type == 3)
+                <div class="issue-list">
+                    <h1><a class="def-link link-info" href="/issue/{{$issue->id}}">{{$issue->title}}</a> <span class="badge  badge-pill badge-secondary">{{$issue->project->name}}</span><span class="badge  badge-pill badge-success">Solved</span></h1>
+                    <h4>{{$issue->description}}</h4>
+                    <h6>Created: {{$issue->created_at}}</h6>
+                    <hr>
+                </div>
+            @endif
+        @endforeach
+    </div>
+</div>
+
+@endsection
