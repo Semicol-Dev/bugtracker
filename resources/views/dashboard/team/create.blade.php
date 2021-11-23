515333173276c71867e4f319083d1e403695f508
cutegrim <48733615+cutegrim@users.noreply.github.com>
2021-11-23 23:47:43 +0100

    Team create style added

--- resources/views/dashboard/team/create.blade.php
+++ resources/views/dashboard/team/create.blade.php
@@ -1,18 +1,90 @@
-<form action="/team" method="post">
-    <!-- CSRF TOKEN, ten tu musi byt lebo CSRF protection je by default zapnuta pri kazdom requeste inak error 413 -->
-    @csrf
-    <p>Nazov Teamu</p>
-    <input type="text" name="name" required><br/>
-    <p>Vyber uzivatelov do teamu</p>
-        <!-- Laravel style foreach, posiem z backendu $users ktora obsahuje vsetkych existujucich uzivatelov ak chces vydiet aky je output z backendu odkomentuj riadok pod tymto-->
-        <!-- {{$users}} -->
-    
-    <select name="members[]" multiple size="6">
-        @foreach ($users as $user)
-            <option value="{{$user->id}}" id="">{{$user->name}}</option>
-        @endforeach
-    </select>
-
+@extends('baseSideNav')
+@section('core')
+<div class="container-fluid ">
     <br>
-    <input type="submit" value="Vytvorit team">
-</form>
+    <br>
+    <div class="row d-flex justify-content-between">
+        <div class="col-3">
+            
+        </div>
+        <div class="col issue-list sm-form-body " >
+            <div class="row d-flex justify-content-center">
+                
+                 <h1>Create Team</h1>
+                  
+            </div>
+            
+            <form action="/team" method="post">
+                
+                <!-- CSRF TOKEN, ten tu musi byt lebo CSRF protection je by default zapnuta pri kazdom requeste inak error 413 -->
+                @csrf
+                <div class="row">
+                    <div class="col-1">
+                        
+                    </div>
+                    <div class="col d-flex justify-content-start">
+                        <h5>Name:</h5>
+                    </div>
+                    
+                </div>
+                <div class="row">
+                    <div class="col-1">
+                        
+                    </div>
+                    <div class="col d-flex justify-content-center" >
+                        <input class=" sm-form-input" type="text" name="name"  required>
+                    </div>
+                    <div class="col-1">
+                            
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-1">
+                        
+                    </div>
+                    <div class="col d-flex justify-content-start">
+                        <h5>Team:</h5>
+                    </div>
+                    
+                </div> 
+                <div class="row">
+                    <div class="col-1">
+                        
+                    </div>
+                    <div class="col d-flex justify-content-center">
+                        <select class=" sm-form-input" name="members[]" multiple size="6">
+                            @foreach ($users as $user)
+                                <option value="{{$user->id}}" id="">{{$user->name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="col-1">
+                        
+                    </div>
+                    
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-1">
+                        
+                    </div>
+                    <div class="col d-flex justify-content-center">
+                        <button type="submit" class="btn btn-info btn-bug-template" style="color: #101010">
+                            <i class="bi bi-plus-circle-fill"></i>Create
+                        </button>
+                    </div>
+                    <div class="col-1">
+                        
+                    </div>
+                    
+                </div>
+            </form>
+
+        </div>
+        <div class="col-3">
+            
+        </div>
+    </div>
+</div>
+@endsection
